c27156c6aff2d6448706a38510ffa81fda7dea44
Andrey Salnikov <a.salnikov.home@gmail.com>
2018-02-20 20:31:27 +0200

    cw4

--- ConsoleApplication14/Program.cs
+++ ConsoleApplication14/Program.cs
@@ -10,6 +10,106 @@ namespace ConsoleApplication14
     {
         static void Main(string[] args)
         {
+
+
+            //float a = float.Parse(Console.ReadLine());
+            //float b = float.Parse(Console.ReadLine());
+            //char c = Convert.ToChar(Console.ReadLine());
+
+            //switch (c)
+            //{
+            //    case '+':
+            //        Console.WriteLine(a + b);
+            //        break;
+            //    case '-':
+            //        Console.WriteLine(a - b);
+            //        break;
+            //    case '*':
+            //        Console.WriteLine(a * b);
+            //        break;
+            //    case '/':
+            //        if (b == 0)
+            //        { Console.WriteLine("You can't divide to 0"); }
+            //        else
+            //        {
+            //            Console.WriteLine(a / b);
+            //        }
+            //        break;
+            //}
+
+
+            //int b = 15;
+            //int c = b == 15 ? 15 : 18;
+
+            //int counter = 0;
+
+
+            //while (counter <=20)
+
+            //{ Console.WriteLine(counter);
+            //    counter++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(counter);
+            //    counter++;
+            //}
+            //while (counter <= 20);
+
+            //for (int i = 0; i <= 15; i++)
+
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int counter = 0;
+
+            //while (counter <= 100)
+            //{
+            //    Console.WriteLine('a');
+            //    counter++;
+
+
+            //}
+
+            //for (int i = 0; i <= 255; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //for (int i = 255; i <= 255&&i>=0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+
+
+
+
+
+            //int count = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i < count; i++)
+            //{
+            //    sum = sum + i;
+
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+
+
+
         }
     }
 }
